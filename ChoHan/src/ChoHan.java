4b8f96dfced58078518d1541e321be4234097a9a
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2018-12-16 17:30:03 -0500

    commit

--- ChoHan/src/ChoHan.java
+++ ChoHan/src/ChoHan.java
@@ -2,8 +2,46 @@ import javax.swing.JOptionPane;
 
 public class ChoHan {
 
-	public static void main(String[] args) {
+	public static <roll, loadedDice> void main(String[] args) {
+		int points1 = 0; 
+		int points2 = 0; 
 		
+		int rolls = 0; 
+		int rolls1 = 0; 
+		
+		for (int i = 0; i < 5; i++) { 
+			int evenvsOdd = (int)(Math.random()* 2) + 1; 
+			if (evenvsOdd == 1) { 
+				Dice regular = new Dice (); 
+				rolls1 = regular.roll(); 
+			}
+			else { 
+				LoadedDice load02 = new LoadedDice(); 
+				rolls1 = load02.roll(); 
+			}
+			String choorhan; if ((rolls + rolls1)%2 ==0) { 
+				choorhan = "Cho"; 
+			}
+			else {
+				choorhan = "Han"; 
+			}
+		String player1 = JOptionPane.showInputDialog(" Player 1: Pick Cho or Han "); 
+		String player2 = JOptionPane.showInputDialog(" Player 2: Pick Cho or Han ");
+			if (player1.equals(choorhan)) {
+				points1 ++; 
+				 JOptionPane.showMessageDialog(null, " Yay Player 1 you got it right! ");
+			}
+			else { 
+				JOptionPane.showMessageDialog(null, " Sorry Player 1 you're wrong ");
+			}
+			if (player2.equals(choorhan)) { 
+				points2 ++; 
+				JOptionPane.showMessageDialog(null, " Yay Player 2 got it right! ");
+			}
+			else { 
+				JOptionPane.showMessageDialog(null, " Sorry player 2 wrong guess ");
+			}
+		}
 
 	}
 
--- ChoHan/src/LoadedDice.java
+++ ChoHan/src/LoadedDice.java
@@ -1,24 +1,39 @@
 
-public class LoadedDice {
+public class LoadedDice extends Dice {
+	private int numSides; 
 	
-	public LoadedDice() {
-		int numSides; 
-	} 
+	
+	public void setnumSides (int ns) { 
+		if (ns == 4 || ns == 6 || ns == 8 || ns == 10 || ns == 12|| ns == 20) { 
+			numSides = ns; 
+		} else { 
+			System.out.println( " invalid # of sides defaulting to 4 ");
+			numSides = 4; 
+		}
+	}
+	public int getnumSides (int ns) { 
+		return numSides; 
+	}
 	
 	public int roll () { 
         // double in the range [0.0, 1.0)
         double r = Math.random();
 
         // integer in the range 1 to 6 with desired probabilities
-        int roll;
-        if      (r < 1.0/8.0) roll = 1;
-        else if (r < 2.0/8.0) roll = 2;
-        else if (r < 3.0/8.0) roll = 3;
-        else if (r < 4.0/8.0) roll = 4;
-        else if (r < 5.0/8.0) roll = 5;
-        else                  roll = 6;
-	return roll; 
-	}	
+        int roll1;
+        if      (r < 1.0/8.0) roll1 = 1;
+        else if (r < 2.0/8.0) roll1 = 2;
+        else if (r < 3.0/8.0) roll1 = 3;
+        else if (r < 4.0/8.0) roll1 = 4;
+        else if (r < 5.0/8.0) roll1 = 5;
+        else                  roll1 = 1;
+        int roll = (int) (roll1* numSides); 
+        return roll; 
+	}
+	public String toString() { 
+		return "\n Side Num " + numSides; 
+	}
+
 }
 
 
