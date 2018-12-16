fe3c4c2992c2b7404a859c03b40a6eb42747e0c9
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2018-12-16 16:23:24 -0500

    initial

--- ChoHan/src/LoadedDice.java
+++ ChoHan/src/LoadedDice.java
@@ -1,8 +1,24 @@
 
 public class LoadedDice {
-
+	
 	public LoadedDice() {
-		
-	}
+		int numSides; 
+	} 
+	
+	public int roll () { 
+        // double in the range [0.0, 1.0)
+        double r = Math.random();
 
+        // integer in the range 1 to 6 with desired probabilities
+        int roll;
+        if      (r < 1.0/8.0) roll = 1;
+        else if (r < 2.0/8.0) roll = 2;
+        else if (r < 3.0/8.0) roll = 3;
+        else if (r < 4.0/8.0) roll = 4;
+        else if (r < 5.0/8.0) roll = 5;
+        else                  roll = 6;
+	return roll; 
+	}	
 }
+
+
