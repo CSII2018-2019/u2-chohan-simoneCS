7a6e4a81625f441f18c49055703ab0066d38cccd
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2018-12-16 13:42:50 -0500

    initial commit

---
+++ ChoHan/src/ChoHan.java
@@ -0,0 +1,13 @@
+
+public class ChoHan {
+
+	public ChoHan() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ ChoHan/src/LoadedDice.java
@@ -0,0 +1,8 @@
+
+public class LoadedDice {
+
+	public LoadedDice() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
